aacb1887331509b60c2fe159f645d0bda39ce6f0
Rick G <lacunainc04@gmail.com>
2016-03-05 19:03:22 -0800

    Add initial front page layout

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,12 @@
  * Controller of the leadditApp
  */
 angular.module('leadditApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function ($scope) {
+    $scope.posts = [
+    {title: 'post 1', upvotes: 1},
+  	{title: 'post 2', upvotes: 12},
+ 	{title: 'post 3', upvotes: 3},
+  	{title: 'post 4', upvotes: 4},
+  	{title: 'post 5', upvotes: 15}
     ];
   });
